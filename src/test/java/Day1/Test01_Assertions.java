9f41dbc8c4a6c8105a83d0134df0f572bf2b2bff
Fatih Deniz <deniz.fatih@hotmail.de>
2023-05-17 22:31:37 +0200

    Day 1 - Unit Test with JUnit

--- src/test/java/Day1/Test01_Assertions.java
+++ src/test/java/Day1/Test01_Assertions.java
@@ -1,2 +1,53 @@
-package Day1;public class Test1_Assertions {
+package Day1;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class Test01_Assertions {
+
+    @Test
+    public void test() {
+        //main metodu yokken nasil calisti???
+        //JUnit icinde gömülü olarak gelen Main metodu cagriliyor
+        //JUnit(Jupiter) ile metodlarin erisim belirleyicisini public yapmak zorunda degiliz.
+    }
+
+    @Test
+    public void testLength() {
+        String cumle = "Merhaba Dünyam"; //14 karakter
+        int anlikDeger = cumle.length();
+        int beklenenDeger = 14;
+        assertEquals(beklenenDeger, anlikDeger);
+    }
+
+    //!!! farkli bir örnek ( toUpperCase() )
+    @Test
+    public void testUpperCase() {
+
+        String anlikDeger = "Merhaba".toUpperCase(); //MERHABA
+        String beklenenDeger = "MERHABA";
+
+        //Testimiz hata verirse, mesaj vermek istersek metodun 3 parametli hali kullanilir
+        assertEquals(beklenenDeger, anlikDeger, "Uppercase metodu düzgün calismadi");
+    }
+
+    @Test
+    public void testToplama() {
+
+        int sayi1 = 10;
+        int sayi2 = 5;
+        int anlikDeger = Math.addExact(sayi1, sayi2);
+        int beklenenDeger = 15;
+        assertEquals(beklenenDeger, anlikDeger);
+
+    }
+
+    @Test
+    public void testContains() {
+
+        assertEquals(false,"Mirac".contains("z"));
+    }
+
 }
--- src/test/java/Day1/Test04_BeforeAll_AfterAll.java
+++ src/test/java/Day1/Test04_BeforeAll_AfterAll.java
@@ -1,2 +1,49 @@
-package Day1;public class Test04_BeforeAll_AfterAll {
+package Day1;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class Test04_BeforeAll_AfterAll {
+
+    @BeforeAll
+    static void beforeAll(){
+
+        System.out.println("open a file");
+    }
+
+
+    @AfterAll
+    static void afterAll(){
+
+        System.out.println("close a file");
+    }
+
+    @Test
+    void testWithArrays(){
+
+        String str = "Unit test with JUnit5";
+
+        String[] anlikDizi = str.split(" ");
+        String[] beklenenDizi = {"Unit","test","with","JUnit5"};
+
+        System.out.println("testWithArray1() calisti");
+
+        assertArrayEquals(beklenenDizi,anlikDizi,"Diziler esit degil");
+    }
+
+    @Test
+    void testWithArrays2(){
+
+        String str = "Unit test with JUnit5";
+
+        String[] anlikDizi = str.split(" ");
+        String[] beklenenDizi = {"Unit","test","with","JUnit5"};
+
+        System.out.println("testWithArray2() calisti");
+
+        assertArrayEquals(beklenenDizi,anlikDizi,"Diziler esit degil");
+    }
 }
